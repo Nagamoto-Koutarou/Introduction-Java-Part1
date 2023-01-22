d10ee4bf85a6f9accd8e273cf64b290d2acc59ce
nagamoto <koutaro.n.1056@gmail.com>
2023-01-22 21:54:06 +0900

    Chapter3の練習問題

---
+++ src/chapter3/Chapter3_2.java
@@ -0,0 +1,14 @@
+package chapter3;
+
+public class Chapter3_2 {
+    public static void main(String[] args) {
+        boolean tenki = false;
+        if (tenki) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩にいきます");
+        } else {
+            System.out.println("DVDを見ます");
+            System.out.println("寝ます");
+        }
+    }
+}
