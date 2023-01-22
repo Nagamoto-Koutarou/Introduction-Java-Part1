90b02ad2ca7cf0b8dd50fd127d5def0b60b4e87a
nagamoto <koutaro.n.1056@gmail.com>
2023-01-22 18:07:51 +0900

    chapter1の練習問題の修正とchapter2の練習問題

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import chapter1.Chapter1_1;
-
 public class Main {
     public static void main(String[] args) {
-        Chapter1_1 chapter1 = new Chapter1_1();
-        chapter1.chapter1();
+        System.out.println("Hello, World!");
     }
 }
+
--- src/chapter1/Chapter1_1.java
+++ src/chapter1/Chapter1_1.java
@@ -1,11 +1,11 @@
 package chapter1;
 
 public class Chapter1_1 {
-    int a = 3;
-    int b = 5;
-    int c = a * b;
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = a * b;
 
-    public void chapter1() {
         System.out.println("縦幅"+ a + "横幅" + b + "の長方形の面積は、" + c);
     }
 }
