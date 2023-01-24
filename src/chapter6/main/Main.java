cafa42a6fbf49662f3800057f576dbfa26bbf579
nagamoto <koutaro.n.1056@gmail.com>
2023-01-24 22:16:37 +0900

    Chapter6の練習問題

---
+++ src/chapter6/main/Main.java
@@ -0,0 +1,15 @@
+package chapter6.main;
+
+import static chapter6.comment.Kouhan.callDeae;
+import static chapter6.comment.Kouhan.showMondokoro;
+import static chapter6.comment.Zenhan.doTogame;
+import static chapter6.comment.Zenhan.doWarusa;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        doWarusa();
+        doTogame();
+        callDeae();
+        showMondokoro();
+    }
+}
